3ddace2d2cab53aa35e72c01a6a7826fbce8129f
DESKTOP-6M7V39N\simon <ss387@hdm-stuttgart.de>
2019-01-06 16:32:32 +0100

    Modellimport

---
+++ Assets/_Project/Scripts/Plattform2Gathering.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Plattform2Gathering : MonoBehaviour {
+
+    public float gatherTime = 6f;
+    float activeGatherDuration = 0f;
+    Image progressionImage;
+    bool platformFinished = false;
+
+    private void Awake()
+    {
+        progressionImage = GetComponentInChildren<Image>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(gatherTime >= activeGatherDuration)
+        {
+            activeGatherDuration += Time.deltaTime;
+
+            var currentFillAmount = activeGatherDuration / gatherTime;
+
+            progressionImage.fillAmount = currentFillAmount;
+        }
+        else if(gatherTime < activeGatherDuration)
+        {
+            progressionImage.fillAmount = 1f;
+            platformFinished = true;
+        }
+	}
+
+    public void ResetPlatform()
+    {
+        activeGatherDuration = 0f;
+        platformFinished = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.name == "PlatformTracker")
+        {
+            if (platformFinished)
+            {
+            ResetPlatform();
+            }
+        }
+    }
+}
